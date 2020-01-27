e9bb3d0c65f4905cabae422886de3d4d9fbaad22
taro-1 <55338866+taro-1@users.noreply.github.com>
2020-01-28 00:03:40 +0900

    Delete HistoryRepository.java

--- demojpa/src/main/java/domain/HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoryRepository extends JpaRepository<History, Integer> {
-}
-
